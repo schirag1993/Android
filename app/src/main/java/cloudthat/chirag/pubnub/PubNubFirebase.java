05d91d0d15bf449751a7ecf7560124e83e826bd1
Chirag S <pears.chirag@gmail.com>
2017-11-15 01:47:11 +0530

    add notification service

---
+++ app/src/main/java/cloudthat/chirag/pubnub/PubNubFirebase.java
@@ -0,0 +1,27 @@
+package cloudthat.chirag.pubnub;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by ChiragS on 14-11-2017.
+ */
+
+public class PubNubFirebase extends FirebaseInstanceIdService {
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("FireBaseID", "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+    private void sendRegistrationToServer(String refreshedToken) {
+        //Send to PubNub/Firebase; I don't know
+        Log.i("FireBaseID", "Token is " + refreshedToken);
+    }
+}
